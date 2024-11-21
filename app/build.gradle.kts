ef0716712baf3bcae54fe722626bb8e14f897ddd
DudkoMatt <github.dudko.matt@mail.ru>
2024-11-21 22:27:51 +0300

    chore: Downgrade minSdk version down to Android 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.github.dudkomatt.androidcourse.chatproject"
-        minSdk = 30
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
